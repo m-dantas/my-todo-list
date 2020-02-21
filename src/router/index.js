af09442da1c6ca4c5e2c74f21dc60719a0d510d3
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2020-02-21 16:53:36 -0300

    add routers

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '@/views/Home'
+import Kanban from '@/views/kanban/Kanban'
+
+Vue.use(Router)
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'kanban',
+    path: '/kanban',
+    component: Kanban
+  }
+]
+
+const router = new Router({ routes })
+
+export default router
